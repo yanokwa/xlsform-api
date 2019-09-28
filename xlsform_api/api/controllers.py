75d064ea68b93159c1d931ce1d61decfb7e7d2bb
Yaw Anokwa <yanokwa@gmail.com>
2019-09-27 21:43:49 -0700

    Output from xlsxform_convert is a mix of errors and warnings

--- xlsform_api/api/controllers.py
+++ xlsform_api/api/controllers.py
@@ -22,30 +22,31 @@ def post():
             xform_fp = open(os.path.join(temp_dir_name, XFORM_FILE_NAME), "w")
             xlsform_fp = open(os.path.join(temp_dir_name, XLSFORM_FILE_NAME), "wb")
             xlsform_fp.write(request.get_data())
-            form_errors = xls2xform.xls2xform_convert(
+            convert_status = xls2xform.xls2xform_convert(
                 xlsform_path=str(xlsform_fp.name),
                 xform_path=str(xform_fp.name),
                 validate=True,
                 pretty_print=False
             )
 
-            if form_errors:
-                logger.warning(form_errors)
+            if convert_status:
+                logger.warning(convert_status)
 
+            # if a file exists, there are no errors
             if os.path.isfile(xform_fp.name):
                 return send_file(xform_fp.name, mimetype=XML_MIME_TYPE)
             else:
-                return format_error(form_errors)
+                return format_status(convert_status)
 
         except Exception as e:
             logger.error(e)
-            return format_error(e)
+            return format_status(e)
             
         finally:
             xform_fp.close()
             xlsform_fp.close()
 
-def format_error(e=GENERIC_ERROR_MESSAGE):
+def format_status(e=GENERIC_ERROR_MESSAGE):
     return jsonify(
         status=BAD_REQUEST_STATUS_CODE,
         message=str(e)
