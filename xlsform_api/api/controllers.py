7d717611cea48b9cca70dc773b8ea3b28ac94052
Yaw Anokwa <yanokwa@gmail.com>
2019-09-30 21:50:48 -0700

    Return file content, avoid the use of finally

--- xlsform_api/api/controllers.py
+++ xlsform_api/api/controllers.py
@@ -16,31 +16,26 @@ GOOD_REQUEST_CODE = 200
 def post():
     with TemporaryDirectory() as temp_dir_name:
         try:
-            xform_fp = open(os.path.join(temp_dir_name, "tmp.xml"), "w")
-            xlsform_fp = open(os.path.join(temp_dir_name, "tmp.xlsx"), "wb")
-            xlsform_fp.write(request.get_data())
-            convert_status = xls2xform.xls2xform_convert(
-                xlsform_path=str(xlsform_fp.name),
-                xform_path=str(xform_fp.name),
-                validate=True,
-                pretty_print=False
-            )
+            with open(os.path.join(temp_dir_name, "tmp.xml"), "w+") as xform, open(os.path.join(temp_dir_name, "tmp.xlsx"), "wb") as xlsform:
+                xlsform.write(request.get_data())
+                convert_status = xls2xform.xls2xform_convert(
+                    xlsform_path=str(xlsform.name),
+                    xform_path=str(xform.name),
+                    validate=True,
+                    pretty_print=False
+                )
 
-            if convert_status:
-                logger.warning(convert_status)
+                if convert_status:
+                    logger.warning(convert_status)
 
-            if os.path.isfile(xform_fp.name):
-                return response(status=GOOD_REQUEST_CODE, result=send_file(xform_fp.name, mimetype="application/xml"), warnings=convert_status)
-            else:
-                return response(status=BAD_REQUEST_CODE, error=convert_status)
+                if os.path.isfile(xform.name):
+                    return response(status=GOOD_REQUEST_CODE, result=xform.read(), warnings=convert_status)
+                else:
+                    return response(status=BAD_REQUEST_CODE, error=convert_status)
 
         except Exception as e:
             logger.error(e)
             return response(status=BAD_REQUEST_CODE, error=str(e))
-            
-        finally:
-            xform_fp.close()
-            xlsform_fp.close()
 
 def response(status=None, result=None, warnings=None, error=None):
     return jsonify(
