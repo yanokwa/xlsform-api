1ca68dec15c562ff2ac78e41214781ba0327bc16
Yaw Anokwa <yanokwa@gmail.com>
2019-09-30 21:19:09 -0700

    Remove unnecessary variables

--- xlsform_api/api/controllers.py
+++ xlsform_api/api/controllers.py
@@ -8,19 +8,16 @@ from pyxform import xls2xform
 api = Blueprint("api", __name__)
 logger = logging.getLogger(__name__)
 
-BAD_REQUEST_STATUS_CODE = 400
-GENERIC_ERROR_MESSAGE = "Error converting XLSForm"
-XLSFORM_FILE_NAME = "tmp.xlsx"
-XFORM_FILE_NAME = "tmp.xml"
-XML_MIME_TYPE = "application/xml"
+BAD_REQUEST_CODE = 400
+GOOD_REQUEST_CODE = 200
 
 # To test: curl --request POST --data-binary @<FILE_NAME>.xlsx http://127.0.0.1:5000/api/v1/convert
 @api.route("/convert", methods=['POST'])
 def post():
     with TemporaryDirectory() as temp_dir_name:
         try:
-            xform_fp = open(os.path.join(temp_dir_name, XFORM_FILE_NAME), "w")
-            xlsform_fp = open(os.path.join(temp_dir_name, XLSFORM_FILE_NAME), "wb")
+            xform_fp = open(os.path.join(temp_dir_name, "tmp.xlsx"), "w")
+            xlsform_fp = open(os.path.join(temp_dir_name, "tmp.xml"), "wb")
             xlsform_fp.write(request.get_data())
             convert_status = xls2xform.xls2xform_convert(
                 xlsform_path=str(xlsform_fp.name),
@@ -34,7 +31,7 @@ def post():
 
             # if a file exists, there are no errors
             if os.path.isfile(xform_fp.name):
-                return send_file(xform_fp.name, mimetype=XML_MIME_TYPE)
+                return send_file(xform_fp.name, mimetype="application/xml")
             else:
                 return format_status(convert_status)
 
@@ -46,8 +43,8 @@ def post():
             xform_fp.close()
             xlsform_fp.close()
 
-def format_status(e=GENERIC_ERROR_MESSAGE):
+def format_status(e=None):
     return jsonify(
-        status=BAD_REQUEST_STATUS_CODE,
+        status=BAD_REQUEST_CODE,
         message=str(e)
-    ), BAD_REQUEST_STATUS_CODE
+    ), BAD_REQUEST_CODE
